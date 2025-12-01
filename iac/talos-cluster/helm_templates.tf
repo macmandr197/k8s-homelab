f0b56e1f7de410fdf4d74f52a951ac0281eb77e2
Andrew MacMillan <andrewwm96@gmail.com>
2025-12-01 17:32:05 +0000

    updates cilium version, autodirects node traffic, updates Proxmox TF provider version

--- iac/talos-cluster/helm_templates.tf
+++ iac/talos-cluster/helm_templates.tf
@@ -3,7 +3,7 @@ data "helm_template" "cilium_template" {
   repository = "https://helm.cilium.io/"
   chart = "cilium"
   namespace  = "kube-system"
-  version = "1.18.3"
+  version = "1.18.4"
   kube_version = local.kubeVersion
 
   set = [
--- iac/talos-cluster/providers.tf
+++ iac/talos-cluster/providers.tf
@@ -7,7 +7,7 @@ terraform {
 
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.86.0"
+      version = "0.88.0"
     }
     talos = {
       source  = "siderolabs/talos"
